424ed8d8708ad90028b31e0218d00d334c5844c0
horsjaco117 <jacob_horsley@live.com>
2025-03-31 08:51:12 -0600

    Changes for string were added

--- GraphicsExamples/GraphicExamplesForm.vb
+++ GraphicsExamples/GraphicExamplesForm.vb
@@ -18,9 +18,45 @@ Public Class GraphicExamplesForm
         g.Dispose()
     End Sub
 
+    Sub drawRectangle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.OrangeRed, 3)
+
+        'pen.Color = Color.PapayaWhip
+
+        g.DrawRectangle(pen, 150, 150, 300, 300)
+
+        g.Dispose()
+    End Sub
+
+    Sub drawcircle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Peru, 45)
+
+        'pen.Color = Color.PapayaWhip
+
+        g.DrawEllipse(pen, 160, 160, 290, 290)
+
+        g.Dispose()
+    End Sub
+    Sub drawstring()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Cornsilk)
+        Dim drawfont As New Font("arial", 16)
+        Dim drawbrush As New SolidBrush(Color.Black)
+
+        'pen.Color = Color.PapayaWhip
+
+        g.DrawString("Graphics!", drawfont, drawbrush, 200, 200)
+
+        g.Dispose()
+    End Sub
     'Event Handlers -----------------------------------------------------------------
     Private Sub GraphicExamplesForm_Click(sender As Object, e As EventArgs) Handles Me.Click
         drawline()
+        drawRectangle()
+        drawcircle()
+        drawstring()
     End Sub
 
     Private Sub GraphicExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove

8490d6e36ef778ebf262bcb240973889f4f053a3
horsjaco117 <jacob_horsley@live.com>
2025-04-02 08:42:03 -0600

    All the things that weren't used are cancelled out

--- GraphicsExamples/GraphicExamplesForm.vb
+++ GraphicsExamples/GraphicExamplesForm.vb
@@ -28,58 +28,58 @@ Public Class GraphicExamplesForm
 
         g.Dispose()
     End Sub
-    Sub drawline()
-        Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.Black)
+    'Sub drawline()
+    '    Dim g As Graphics = Me.CreateGraphics
+    '    Dim pen As New Pen(Color.Black)
 
-        'pen.Color = ForeGroundColor(Color.Lime)
+    '    'pen.Color = ForeGroundColor(Color.Lime)
 
-        g.DrawLine(pen, 50, 50, 100, 100)
+    '    g.DrawLine(pen, 50, 50, 100, 100)
 
-        g.Dispose()
-    End Sub
+    '    g.Dispose()
+    'End Sub
 
-    Sub drawRectangle()
-        Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.OrangeRed, 3)
+    'Sub drawRectangle()
+    '    Dim g As Graphics = Me.CreateGraphics
+    '    Dim pen As New Pen(Color.OrangeRed, 3)
 
-        'pen.Color = Color.PapayaWhip
+    '    'pen.Color = Color.PapayaWhip
 
-        g.DrawRectangle(pen, 150, 150, 300, 300)
+    '    g.DrawRectangle(pen, 150, 150, 300, 300)
 
-        g.Dispose()
-    End Sub
+    '    g.Dispose()
+    'End Sub
 
-    Sub drawcircle()
-        Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.Peru, 45)
+    'Sub drawcircle()
+    '    Dim g As Graphics = Me.CreateGraphics
+    '    Dim pen As New Pen(Color.Peru, 45)
 
-        'pen.Color = Color.PapayaWhip
+    '    'pen.Color = Color.PapayaWhip
 
-        g.DrawEllipse(pen, 160, 160, 290, 290)
+    '    g.DrawEllipse(pen, 160, 160, 290, 290)
 
-        g.Dispose()
-    End Sub
-    Sub drawstring()
-        Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.Cornsilk)
-        Dim drawfont As New Font("arial", 16)
-        Dim drawbrush As New SolidBrush(ForeGroundColor)
+    '    g.Dispose()
+    'End Sub
+    'Sub drawstring()
+    '    Dim g As Graphics = Me.CreateGraphics
+    '    Dim pen As New Pen(Color.Cornsilk)
+    '    Dim drawfont As New Font("arial", 16)
+    '    Dim drawbrush As New SolidBrush(ForeGroundColor)
 
-        'pen.Color = Color.PapayaWhip
+    '    'pen.Color = Color.PapayaWhip
 
-        g.DrawString("Graphics!", drawfont, drawbrush, 200, 200)
+    '    g.DrawString("Graphics!", drawfont, drawbrush, 200, 200)
 
-        g.Dispose()
-    End Sub
+    '    g.Dispose()
+    'End Sub
     'Event Handlers -----------------------------------------------------------------
-    Private Sub GraphicExamplesForm_Click(sender As Object, e As EventArgs) 'Handles Me.Click
-        Me.Refresh()
-        drawline()
-        drawRectangle()
-        drawcircle()
-        drawstring()
-    End Sub
+    'Private Sub GraphicExamplesForm_Click(sender As Object, e As EventArgs) 'Handles Me.Click
+    '    Me.Refresh()
+    '    drawline()
+    '    drawRectangle()
+    '    drawcircle()
+    '    drawstring()
+    'End Sub
 
     Private Sub GraphicExamplesForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove, Me.MouseDown
         Static oldX, oldY As Integer

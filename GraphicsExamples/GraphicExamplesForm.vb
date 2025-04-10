107ac367b464b3724d91704145e03db71296bd30
horsjaco117 <jacob_horsley@live.com>
2025-04-10 08:28:59 -0600

    add draw vertical line

--- GraphicsExamples/GraphicExamplesForm.vb
+++ GraphicsExamples/GraphicExamplesForm.vb
@@ -109,9 +109,14 @@ Public Class GraphicExamplesForm
                 DrawWithMouse(oldX, oldY, e.X, e.Y)
             Case "Right"
                 'MsgBox("Yippee!!")
+
                 'ignore and use context menu
             Case "Middle"
                 'TODO
+                '
+                ''DrawWithMouse(DrawingPictureBox.Width \ 2, 0, DrawingPictureBox.Width \ 2, DrawingPictureBox.Height)
+                'draw a line top to bottom on the current mouse x location
+                DrawWithMouse(e.X, 0, e.X, DrawingPictureBox.Height)
         End Select
         Me.Text = $"({e.X},{e.Y}) {e.Button.ToString} FG {ForeGroundColor.ToString}"
 
